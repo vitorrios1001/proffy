04be94e6747e7e3deaea8934be19e6bdf8ea2b48
Vitor Rios <vitor.neto@br.experian.com>
2020-08-09 03:09:01 -0300

    Adding providers to index application

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 
+import { ConnectionsProvider } from './providers/connectionsProvider';
+import { TeacherProvider } from './providers/teacherProvider';
+
 import './assets/styles/global.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ConnectionsProvider>
+      <TeacherProvider>
+        <Routes />
+      </TeacherProvider>
+    </ConnectionsProvider>
+
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
