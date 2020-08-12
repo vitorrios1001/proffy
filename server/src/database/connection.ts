b57d2ec6d8fedd4f5719ff010457ab3732a7774d
Vitor Rios <vitor.neto@br.experian.com>
2020-08-12 17:43:18 -0300

    Adding configuration to migration

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,10 +1,14 @@
 import knex from 'knex'
 import path from 'path'
 
+const filename = process.env.NODE_ENV === 'production'
+  ? path.resolve(__dirname, '..', '..', '..', 'database', 'database.sqlite')
+  : path.resolve(__dirname, '..', '..', 'database', 'database.sqlite')
+
 const db = knex({
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
+    filename,
   },
   useNullAsDefault: true,
 })
