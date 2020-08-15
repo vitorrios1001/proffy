f1c8e3ed33725af28f6885ea6fc56ff8e28ee8a7
Vitor Rios <vitor.neto@br.experian.com>
2020-08-15 12:34:57 -0300

    Ajusting consult to  classes

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -5,34 +5,38 @@ import { db } from "../database/connection"
 
 class ClassesController {
   async index(req: Request, res: Response) {
-    const filters = req.query
+    try {
+      const filters = req.query
 
-    const subject = filters.subject as string
-    const week_day = Number(filters.week_day)
-    const time = filters.time as string
+      const subject = filters.subject as string
+      const week_day = Number(filters.week_day)
+      const time = filters.time as string
 
-    if (week_day < 0 || !Boolean(subject) || !Boolean(time)) {
-      return res.status(400).json({
-        error: 'Missing filters to search classes',
-      })
+      if (week_day < 0 || !Boolean(subject) || !Boolean(time)) {
+        return res.status(400).json({
+          error: 'Missing filters to search classes',
+        })
+      }
+
+      const timeInMinutes = convertHourToMinutes(time)
+
+      const classes = await db('classes')
+        .whereExists(function() {
+          this.select('*')
+            .from('class_schedule')
+            .whereRaw('class_schedule.class_id = classes.id')
+            .whereRaw('class_schedule.week_day = ??', [week_day])
+            .whereRaw('class_schedule.from <= ??', [timeInMinutes])
+            .whereRaw('class_schedule.to > ??', [timeInMinutes])
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select(['classes.*', 'users.*'])
+
+      return res.status(200).json(classes)
+    } catch (error) {
+      return res.status(400).json({ message: error })
     }
-
-    const timeInMinutes = convertHourToMinutes(time)
-
-    const classes = await db('classes')
-      .whereExists(function() {
-        this.select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [week_day])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*'])
-
-    return res.status(200).json(classes)
   }
   
   async create(req: Request, res: Response) {
@@ -54,13 +58,13 @@ class ClassesController {
         avatar,
         whatsapp,
         bio,
-      })
-  
+      }).returning('id')
+
       const [class_id] = await trx('classes').insert({
         subject,
         cost,
         user_id,
-      })
+      }).returning('id')
   
       const classesSchedule = schedule.map((scheduleItem: ScheduleItem) => ({
         week_day: scheduleItem.week_day,
@@ -73,6 +77,7 @@ class ClassesController {
   
       await trx.commit()
     } catch (error) {
+      console.log(error)
       await trx.rollback()
       
       return res.status(400).json({
