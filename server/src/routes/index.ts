a18ddd4277ed34a48a4027ffaeaf7a23ab6ea975
Vitor Rios <vitor.neto@br.experian.com>
2020-08-15 20:14:29 -0300

    Implementing  recaptcha on backend

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { ClassesController } from '../controllers/ClassesController'
 import { ConnectionsController } from '../controllers/ConnectionsController'
 
+import { validateRecaptcha } from '../middlewares/recaptcha'
+
 const routes = express.Router()
 const classesController = new ClassesController()
 const connectionsController = new ConnectionsController()
@@ -9,7 +11,7 @@ const connectionsController = new ConnectionsController()
 routes.get('/', (req, res) => res.status(200).json({ status: 'Online api' }))
 
 routes.get('/classes', classesController.index )
-routes.post('/classes', classesController.create )
+routes.post('/classes', validateRecaptcha, classesController.create )
 
 routes.get('/connections', connectionsController.index )
 routes.post('/connections', connectionsController.create )
