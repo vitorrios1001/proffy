332e8163cc31d69acc16e27c3299cc4cf0f556ae
Vitor Rios <vitor.neto@br.experian.com>
2020-08-15 10:54:03 -0300

    Separating server and listener

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
-import config from './config'
 
 const app = express()
 
@@ -9,8 +8,5 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-const PORT = config.SERVER_PORT
 
-app.listen(PORT, () => {
-  console.log(`Running in port: ${PORT}`)
-})
+export default app
