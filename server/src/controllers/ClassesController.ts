7a565be0734bcc572aef4e329b36035d5ec440c6
Vitor Rios <vitor.neto@br.experian.com>
2020-08-28 03:28:32 -0300

    Refactoriong controllers to new architecture

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express"
-import { convertHourToMinutes } from "../utils/convertHourToMinutes"
-import { ScheduleItem } from "../models/Schedule"
-import { db } from "../database/connection"
+import { ClassesService } from "~/services/ClassesService"
 
 class ClassesController {
+  constructor(
+    private classesService: ClassesService,
+  ) { }
+
   async index(req: Request, res: Response) {
     try {
       const filters = req.query
@@ -12,80 +14,22 @@ class ClassesController {
       const week_day = Number(filters.week_day)
       const time = filters.time as string
 
-      if (week_day < 0 || !Boolean(subject) || !Boolean(time)) {
-        return res.status(400).json({
-          error: 'Missing filters to search classes',
-        })
-      }
-
-      const timeInMinutes = convertHourToMinutes(time)
-
-      const classes = await db('classes')
-        .whereExists(function() {
-          this.select('*')
-            .from('class_schedule')
-            .whereRaw('class_schedule.class_id = classes.id')
-            .whereRaw('class_schedule.week_day = ??', [week_day])
-            .whereRaw('class_schedule.from <= ??', [timeInMinutes])
-            .whereRaw('class_schedule.to > ??', [timeInMinutes])
-        })
-        .where('classes.subject', '=', subject)
-        .join('users', 'classes.user_id', '=', 'users.id')
-        .select(['classes.*', 'users.*'])
+      const classes = await this.classesService.getClasses(week_day, subject, time)
 
       return res.status(200).json(classes)
     } catch (error) {
-      return res.status(400).json({ message: error })
+      return res.status(400).json({ message: error.message || 'Unexpected error' })
     }
   }
-  
-  async create(req: Request, res: Response) {
-    const {
-      name,
-      avatar,
-      whatsapp,
-      bio,
-      subject,
-      cost,
-      schedule
-    } = req.body
-    
-    const trx = await db.transaction()
-    
-    try {
-      const [user_id] = await trx('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-      }).returning('id')
 
-      const [class_id] = await trx('classes').insert({
-        subject,
-        cost,
-        user_id,
-      }).returning('id')
-  
-      const classesSchedule = schedule.map((scheduleItem: ScheduleItem) => ({
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-        class_id,
-      }))
-  
-      await trx('class_schedule').insert(classesSchedule)
-  
-      await trx.commit()
+  async create(req: Request, res: Response) {
+    try {
+      await this.classesService.createClass(req.body)
+
+      return res.status(201).json({ message: 'Class created with success' })
     } catch (error) {
-      console.log(error)
-      await trx.rollback()
-      
-      return res.status(400).json({
-        error: 'Unexpected error while creating new class'
-      })
+      return res.status(400).json({ message: error.message || 'Unexpected error' })
     }
-  
-    return res.status(201).json({ message: 'Class created with success' })
   }
 }
 
