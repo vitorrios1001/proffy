b402a1ebcadc2c6b7f978b0829d589dff2073797
Vitor Rios <vitor.neto@br.experian.com>
2020-09-09 19:56:40 -0300

    Adding new test of   integration  to connections model

--- server/__tests__/integration/connections.test.ts
+++ server/__tests__/integration/connections.test.ts
@@ -1,11 +1,15 @@
 import request from 'supertest'
 import app from '../../src/server'
 import { truncate } from '../utils/database'
+import { aleatory } from '../factories'
+import { ClassesRepository } from '../../src/repositories/implementations/ClassesRepository'
 
 describe('Connections route', () => {
   beforeEach(async () => {
     await truncate()
   })
+
+  const classesRepository = new ClassesRepository()
 
   it('should be get total of connections', async () => {
     const resp = await request(app).get('/connections')
@@ -15,6 +19,32 @@ describe('Connections route', () => {
 
   it('should be error to create a connection without user_id', async () => {
     const resp = await request(app).post('/connections')
+
+    expect(resp.body.message).toBe('Field user_id is required')
+  })
+
+  it('should be to create a connection with success', async () => {
+    const user = aleatory.user()
+    const subject = aleatory.subject()
+    const cost = aleatory.cost()
+    const schedule = []
+
+    const week = [1,3,4]
+    
+    week.forEach(week_day => {
+      const item = {
+        week_day,
+        from: aleatory.time(8),
+        to: aleatory.time(18),
+      }
+      schedule.push(item)
+    });
+
+    await classesRepository.save(user, subject, cost, schedule)
+
+    await classesRepository.getClassesFiltered(1, subject, 9 * 60)
+
+    const resp = await request(app).post('/connections').send({ user_id: 1 })
 
     expect(resp.status).toBe(400)
   })
